d2c67d13d6e267cb382d056a07738e9ba100d10f
Punsano26 <xlichaphlreuxn@gmail.com>
2024-08-05 17:25:02 +0700

    update relation

--- models/role.model.js
+++ models/role.model.js
@@ -3,12 +3,12 @@ const sequelize = require("./db");
 //define DB Schema
 const Role = sequelize.define("role", {
   id: { //ถ้าเกิดเราไม่ได้กำหนด primary key มันจะทำให้ id เป็นค่า defult ของ sequelize
-    type: DataTypes.INTEGER,
+    type: DataTypes.INTEGER, // ชนิดตัวเลข
     primaryKey: true,
     autoIncrement: true,
   },
   name: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING, // ชนิดตัวอักษร 
     allowNull: false,
   },
 });
